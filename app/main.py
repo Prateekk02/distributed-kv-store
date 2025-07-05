fec0435e86e0c754cbe73ffc1f53b6c45b113b18
Prateekk02 <prateek.kumar2002@gmail.com>
2025-07-05 15:54:20 +0530

    Added Dependency injection

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException, Depends
 from .database import get_connection, create_kv_table
 from contextlib import asynccontextmanager
 
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     get_connection()
@@ -11,49 +13,38 @@ async def lifespan(app: FastAPI):
     yield
     print("Shutting down app")
 
-
+def get_db():
+    conn = get_connection()
+    cur = conn.cursor()
+    try:
+        yield cur
+        conn.commit()
+    finally:
+        cur.close()
+        conn.close()
+        
+        
 app = FastAPI(lifespan=lifespan)
 
 @app.put("/kv/{key}")
-def put_key(key:str, value:str):
-    conn = get_connection()
-    cur = conn.cursor()
-         
-    try:
-        cur.execute("INSERT INTO kv_store (key, value) VALUES (%s, %s) ON CONFLICT (key) DO UPDATE SET VALUE = EXCLUDED.value;", (key, value))
-        conn.commit()
-        return {"message": "Key stored"}
-    finally: 
-        cur.close()
-        conn.close()
+def put_key(key:str, value:str, cur = Depends(get_db)):
+    cur.execute("INSERT INTO kv_store (key, value) VALUES (%s, %s) ON CONFLICT (key) DO UPDATE SET VALUE = EXCLUDED.value;", (key, value))
+    return {"message": "Key stored"}
+   
         
 @app.get("/kv/{key}")
-def get_key(key:str):
-    conn = get_connection()
-    cur = conn.cursor()
+def get_key(key:str, cur = Depends(get_db)):   
+    cur.execute("SELECT value FROM kv_store WHERE key = %s;", (key,))
+    row = cur.fetchone()
+    if row is None:
+        raise HTTPException(status_code=404, detail="Key not found")        
+    return {"value", row[0]}
     
-    try:
-        cur.execute("SELECT value FROM kv_store WHERE key = %s;", (key,))
-        row = cur.fetchone()
-        if row is None:
-            raise HTTPException(status_code=404, detail="Key not found")
-        
-        return {"value", row[0]}
-    finally:
-        cur.close()
-        conn.close()
         
 @app.delete("/kv/{key}")
-def delete_key(key:str):
-    conn = get_connection()
-    cur = conn.cursor()
-    
-    try:
-        cur.execute("DELETE FROM kv_store WHERE key = %s;", (key,))
-        if cur.rowcount == 0:
-            raise HTTPException(status_code=404, detail="Key not found")
-        conn.commit()
-        return {"message": "Key deleted successfully"}
-    finally:
-        cur.close()
-        conn.close()
+def delete_key(key:str, cur = Depends(get_db)):   
+    cur.execute("DELETE FROM kv_store WHERE key = %s;", (key,))
+    if cur.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Key not found")    
+    return {"message": "Key deleted successfully"}
+   
